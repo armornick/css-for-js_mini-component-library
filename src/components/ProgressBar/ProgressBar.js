9db8389a6068d8d25f5f904339615ff05de375af
armornick <nickstories@gmail.com>
2024-12-05 12:46:14 +0100

    finished ProgressBar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -20,19 +20,30 @@ const SIZES = {
 
 const ProgressBar = ({ value, size }) => {
   return (
-    <Progress max={100} value={value} style={SIZES[size]}>
-      {value}
-    </Progress>
+    <Wrapper role="progressbar" aria-valuenow={value} style={SIZES[size]}>
+      <ValueBar value={value}>
+        <VisuallyHidden>Current Value: {value}</VisuallyHidden>
+      </ValueBar>
+    </Wrapper>
   );
 };
 
-const Progress = styled.progress`
-  color: ${COLORS.primary}
+const Wrapper = styled.div`
   background-color: ${COLORS.transparentGray15}
   border-radius: 4px;
   box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
   height: var(--progress-height);
   padding: var(--progress-padding);
+`;
+
+const ValueBar = styled.div`
+  width: ${(props) => props.value}%;
+  height: 100%;
+  background-color: ${COLORS.primary};
+  border-top-left-radius: 4px;
+  border-bottom-left-radius: 4px;
+  border-top-right-radius: ${(props) => (props.value > 99 ? "4px" : "0px")};
+  border-bottom-right-radius: ${(props) => (props.value > 99 ? "4px" : "0px")};
 `;
 
 export default ProgressBar;

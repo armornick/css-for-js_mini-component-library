fb8945ec4ac8353a1b31387116b141eddde3c931
armornick <nickstories@gmail.com>
2024-12-05 13:47:12 +0100

    finished Select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,52 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper style={{ "--value-char-count": displayedValue.length }}>
+      <SelectElement value={value} onChange={onChange}>
+        {children}
+      </SelectElement>
+      <Icon
+        style={{
+          position: "absolute",
+          top: "25%",
+          bottom: "25%",
+          right: "16px",
+          height: "50%",
+          pointerEvents: "none",
+        }}
+        id="chevron-down"
+      />
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  display: inline-block;
+  color: ${COLORS.gray700};
+  background-color: ${COLORS.transparentGray15};
+  border-radius: 8px;
+  width: calc((var(--value-char-count) * 1em) + 16px);
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const SelectElement = styled.select`
+  appearance: none;
+  background-color: transparent;
+  padding: 16px;
+  border: none;
+  width: 100%;
+`;
 
 export default Select;
